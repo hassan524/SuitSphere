3abaaefa5a6879fccf8170e04b7fd66205a32050
Hassan Rehan <hrehan752@gmail.com>
2025-02-19 23:09:50 +0500

    Update authController.ts

--- backend/src/controller/authController.ts
+++ backend/src/controller/authController.ts
@@ -54,10 +54,7 @@ export const UserLogin = async (req: Request, res: Response) => {
     );
 
     res.cookie('HToken', token, {
-      expires: new Date(Date.now() + 5 * 24 * 60 * 60 * 1000),
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'None',
+      expires: new Date(Date.now() + 5 * 24 * 60 * 60 * 1000)
     });
 
     res.status(200).json({ message: 'Successfully logged in', user: user });
